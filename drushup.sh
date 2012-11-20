6bee8bb655147c75669094593eb9edbfeabc4ece
Scott Reeves <cottser@mailworks.org>
2012-11-19 20:37:07 -0500

    Revising Drush update script

--- drushup.sh
+++ drushup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-git subtree pull --prefix=bin/extras/drush --squash -m "Updating drush" http://git.drupal.org/project/drush.git master
+echo "Updating Drush…"
+cd "$(dirname "$0")/bin/extras/drush"
+git pull
